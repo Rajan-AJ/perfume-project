a3b19d4b4559dc71341b4a23c033baa30fe2159a
Rajan-AJ <antonyrajan107@gmail.com>
2024-10-20 19:23:45 +0530

    update perfume project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Navbar from './Componenets/Navbar'
+import Search from './Componenets/Search';
+import Product from './Componenets/Product';
+import About from './Componenets/About';
+import Footer from './Componenets/Footer';
+
+
+function App() {
+    return (
+        <>
+            <Navbar></Navbar>
+            <Search></Search>
+            <Product></Product>
+            <About></About>
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default App
